94a1e49346c9edf227d7477575e0d0978a516312
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-09 02:09:55 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let array_1 = [
+    23,
+    54,
+    75
+]
+
+console.log(array_1)
+
+array_1.push(11, 32, 42, 5, 71)
+console.log(array_1)
+
+array_1.pop(3, 4);
+console.log(array_1)
+
+array_1.shift(0,1)
+console.log(array_1)
